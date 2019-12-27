6583d4e1d84b62c386440d05f47a3232c6688cb5
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-12-27 13:05:24 -0600

    Updated Peer-2-Pocket Monster's description

--- src/app/list-portfolio-projects/projects-list.ts
+++ src/app/list-portfolio-projects/projects-list.ts
@@ -445,7 +445,7 @@ export const PROJECTLIST: PortfolioProject[] = [
         previewLine1: "Monster Battling App",
         previewLine2: "Peer-to-Peer Battles In-Browser",
         longDesc:
-            "~",
+            "A browser based, peer-to-peer monster battle simulator. On the website, players can select characters and connect to one another directly to play. Players then take turns selecting moves in an attempt to bring their opponent’s health to 0 HP. There are a variety of monsters to choose from, each with their own set of moves and specializations. While battling, players can also use a chat feature to send messages back and forth. The app is fully mobile responsive and works across all platforms. In the team in CSCI 415: Networking and Parallel Computation, I was the primary front-end designer, gameplay designer, and systems architect. I also advised other group members on the usage of the PeerJS library and how to incorporate it into their code. (If you would like to test the game alone, simply open the site in two tabs!)",
         techUsed: "HTML5, SCSS, JavaScript, PeerJS",
         modalMediaPath: "/assets/img/portfolio/peer2pocket.jpg",
         modalIsImage: true,
